9353162ecfbd5afbbcc7a1b65f019188c71d81a7
Brian Denlinger <brian.denlinger1@gmail.com>
2019-03-22 11:28:30 -0400

    DB work
    Created blank DB and debugged init script

--- TrashCAN_API/database/init_db.sql
+++ TrashCAN_API/database/init_db.sql
@@ -1,51 +1,47 @@
-CREATE TABLE System_Option (
+CREATE TABLE System_Options (
 	system_option_id integer PRIMARY KEY AUTOINCREMENT,
 	option_name varchar,
 	option_value varchar
 );
 
-CREATE TRIGGER trg_System_Options_Delete AFTER DELETE ON System_Options
-BEGIN
-  INSERT INTO System_Option_Changes (system_option_id,change_type,old_option_name,old_option_value)
-  SELECT system_option_id,'D',option_name,option_value
-  FROM deleted
-END
-
-CREATE TRIGGER trg_System_Options_Insert AFTER INSERT ON System_Options
-BEGIN
-  INSERT INTO System_Option_Changes (system_option_id,change_type,old_option_name,old_option_value)
-  SELECT system_option_id,'I',option_name,option_value
-  FROM inserted
-END
-
-CREATE TRIGGER trg_System_Options_Update AFTER UPDATE ON System_Options
-BEGIN
-  INSERT INTO System_Option_Changes (system_option_id,change_type,option_name,old_option_name,option_value,old_option_value)
-  SELECT system_option_id,'U',inserted.option_name,deleted.option_name,inserted.option_value,deleted.option_value
-  FROM inserted INNER JOIN deleted ON deleted.system_option_id = inserted.system_option_id
-END
-
-
 CREATE TABLE System_Option_Changes (
 	system_option_change_id integer PRIMARY KEY AUTOINCREMENT,
 	system_option_id integer,
-	timestamp datetime,
+	timestamp datetime DEFAULT CURRENT_TIMESTAMP,
 	change_type varchar,
 	option_name varchar,
-	old_option_name varchar,
+	Old_option_name varchar,
 	option_value varchar,
-	old_option_value varchar
+	Old_option_value varchar
 );
 
-CREATE TABLE barcode (
+CREATE TABLE Barcode (
 	barcode_id varchar,
-	timestamp datetime,
+	timestamp datetime DEFAULT CURRENT_TIMESTAMP,
 	barcode varchar
 );
 
-CREATE TABLE weight (
+CREATE TABLE Weight (
 	weight_id varchar,
-	timestamp datetime,
+	timestamp datetime DEFAULT CURRENT_TIMESTAMP,
 	weight integer,
 	weight_raw varchar
 );
+
+CREATE TRIGGER trg_System_Options_Delete AFTER DELETE ON System_Options
+BEGIN
+  INSERT INTO System_Option_Changes (system_option_id,change_type,Old_option_name,Old_option_value)
+  SELECT Old.system_option_id,'D',Old.option_name,Old.option_value;
+END;
+
+CREATE TRIGGER trg_System_Options_Insert AFTER INSERT ON System_Options
+BEGIN
+  INSERT INTO System_Option_Changes (system_option_id,change_type,option_name,option_value)
+  SELECT New.system_option_id,'I',New.option_name,New.option_value;
+END;
+
+CREATE TRIGGER trg_System_Options_Update AFTER UPDATE ON System_Options
+BEGIN
+  INSERT INTO System_Option_Changes (system_option_id,change_type,option_name,Old_option_name,option_value,Old_option_value)
+  SELECT New.system_option_id,'U',New.option_name,Old.option_name,New.option_value,Old.option_value;
+END;
