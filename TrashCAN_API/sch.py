318a35b06b506b249cee97d5ff787ea4e4a9e16c
Brian Denlinger <brian.denlinger1@gmail.com>
2019-04-05 09:51:01 -0400

    Rewrote sch module
    Weworked sch module to only have 3 possible cleaning jobs. 2 default cycle length jobs and 1 custom length job.

--- TrashCAN_API/sch.py
+++ TrashCAN_API/sch.py
@@ -9,49 +9,20 @@ import config
 # Scheduler
 class Config:
     JOBS = [
-        {'id' : 'long_cycle_uvc',
-         'func' : 'app:job1',
-         'trigger' : 'cron',
-         'hour' : config.conf['long_cycle_uvc_minute'],
-         'minute' : config.conf['long_cycle_uvc_minute']
-        },
-        {'id': 'short_cycle_uvc',
-         'func': 'app:job2',
+        {'id': 'custom_cycle_both',
+         'func': 'sch:custom_cycle',
+         'args': '300',
          'trigger': 'cron',
-         'hour': config.conf['short_cycle_uvc_hour'],
-         'minute': config.conf['short_cycle_uvc_minute']
-        },
-        {'id': 'long_cycle_fan',
-         'func': 'app:job3',
-         'trigger': 'cron',
-         'hour': config.conf['long_cycle_fan_hour'],
-         'minute': config.conf['long_cycle_fan_minute']
-        },
-        {'id': 'short_cycle_fan',
-         'func': 'app:job4',
-         'trigger': 'cron',
-         'hour': config.conf['short_cycle_fan_hour'],
-         'minute': config.conf['short_cycle_fan_minute',]
-        },
-        {'id': 'long_cycle_both',
-         'func': 'app:job5',
-         'trigger': 'cron',
-         'hour': config.conf['long_cycle_both_hour'],
-         'minute': config.conf['long_cycle_both_minute']
-        },
-        {'id': 'short_cycle_both',
-         'func': 'app:job6',
-         'trigger': 'cron',
-         'hour': config.conf['short_cycle_both_hour'],
-         'minute': config.conf['short_cycle_both_both',]
+         'hour': '20',
+         'minute': '30'
         },
         {'id' : 'phone_home',
-         'func' : 'app:job7',
+         'func' : 'sch:phone_home',
          'trigger' : 'interval',
          'seconds' : config.conf['phone_home_sleep']
         },
         {'id' : 'broadcast_location',
-         'func' : 'app:job8',
+         'func' : 'sch:broadcast',
          'trigger' : 'interval',
          'seconds' : config.conf['broadcast_sleep']
         }
@@ -68,60 +39,8 @@ class Config:
     }
     SCHEDULER_API_ENABLED = True
 
-    # Long cycle UVC
-    def job1(self):
-        if requests.get('http://127.0.0.1/api/light').text == 'on':
-            logging.warning("Light already on")
-            return
-        responses = list()
-        responses.append(requests.put('http://127.0.0.1/api/light?action=on'))
-        sleep(config['LONG_CYCLE_SLEEP'])
-        responses.append(requests.put('http://127.0.0.1/api/light?action=off'))
-        for index, response in responses:
-            if response is not '200':
-                logging.warning("Error from API: %s", responses[index])
-
-    # Short cycle UVC
-    def job2(self):
-        if requests.get('http://127.0.0.1/api/light').text == 'on':
-            logging.warning("Light already on")
-            return
-        responses = list()
-        responses.append(requests.put('http://127.0.0.1/api/light?action=on'))
-        sleep(config['SHORT_CYCLE_SLEEP'])
-        responses.append(requests.put('http://127.0.0.1/api/light?action=off'))
-        for index, response in responses:
-            if response is not '200':
-                logging.warning("Error from API: %s", responses[index])
-
-    # Long cycle fan
-    def job3(self):
-        if requests.get('http://127.0.0.1/api/fan').text == 'on':
-            logging.warning("Fan already on")
-            return
-        responses = list()
-        responses.append(requests.put('http://127.0.0.1/api/fan?action=on'))
-        sleep(config['LONG_CYCLE_SLEEP'])
-        responses.append(requests.put('http://127.0.0.1/api/fan?action=off'))
-        for index, response in responses:
-            if response is not '200':
-                logging.warning("Error from API: %s", responses[index])
-
-    # Short cycle fan
-    def job4(self):
-        if requests.get('http://127.0.0.1/api/fan').text == 'on':
-            logging.warning("Fan already on")
-            return
-        responses = list()
-        responses.append(requests.put('http://127.0.0.1/api/fan?action=on'))
-        sleep(config['SHORT_CYCLE_SLEEP'])
-        responses.append(requests.put('http://127.0.0.1/api/fan?action=off'))
-        for index, response in responses:
-            if response is not '200':
-                logging.warning("Error from API: %s", responses[index])
-
     # Long cycle both
-    def job5(self):
+    def long_cycle(self):
         if requests.get('http://127.0.0.1/api/fan').text == 'on':
             logging.warning("Fan already on")
             return
@@ -139,7 +58,7 @@ class Config:
                 logging.warning("Error from API: %s", responses[index])
 
     # Short cycle both
-    def job6(self):
+    def short_cycle(self):
         if requests.get('http://127.0.0.1/api/fan').text == 'on':
             logging.warning("Fan already on")
             return
@@ -156,8 +75,26 @@ class Config:
             if response is not '200':
                 logging.warning("Error from API: %s", responses[index])
 
+    # Long cycle both
+    def custom_cycle(self, length):
+        if requests.get('http://127.0.0.1/api/fan').text == 'on':
+            logging.warning("Fan already on")
+            return
+        if requests.get('http://127.0.0.1/api/light').text == 'on':
+            logging.warning("Light already on")
+            return
+        responses = list()
+        responses.append(requests.put('http://127.0.0.1/api/fan?action=on'))
+        responses.append(requests.put('http://127.0.0.1/api/light?action=on'))
+        sleep(length)
+        responses.append(requests.put('http://127.0.0.1/api/fan?action=off'))
+        responses.append(requests.put('http://127.0.0.1/api/light?action=off'))
+        for index, response in responses:
+            if response is not '200':
+                logging.warning("Error from API: %s", responses[index])
+
     # Phone home to AWS server. Attempt to upload any stored barcodes and weight measurements.
-    def job7(self):
+    def phone_home(self):
         logging.debug('Phone home to server started')
 
         r = requests.get('http://127.0.0.1/api/weight')
@@ -190,7 +127,7 @@ class Config:
         logging.debug('Phone home to server complete')
 
     # Broadcast location to wi-fi
-    def job8(self):
+    def broadcast(self):
         logging.debug('WiFi broadcast started.')
         msg = socket.gethostbyname(socket.gethostname())
 
